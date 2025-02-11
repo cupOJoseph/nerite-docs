54878780fcb1bcc631066028675b38e3fb5931be
Joseph Schiarizzi <9449596+cupOJoseph@users.noreply.github.com>
2025-02-11 11:15:17 -0500

    add build

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   url: 'https://neriteorg.github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/neriteorg.github.io/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
