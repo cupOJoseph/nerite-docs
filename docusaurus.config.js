17d5288c7b48a3b989d58782de04b40fdabeb263
Joseph Schiarizzi <9449596+cupOJoseph@users.noreply.github.com>
2025-02-11 11:23:33 -0500

    new build

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://neriteorg.github.com',
+  url: 'https://docs.nerite.org',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
